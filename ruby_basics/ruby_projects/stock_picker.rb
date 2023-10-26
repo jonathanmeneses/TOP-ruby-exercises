447347d1459daa129232abe901aa151e1dd9f392
Jonathan Meneses <jonathan.m.meneses@gmail.com>
2023-10-25 23:53:47 -0500

    Inefficient Solution to stock_picker

---
+++ ruby_basics/ruby_projects/stock_picker.rb
@@ -0,0 +1,18 @@
+# Method to pick the two days where there is a greatest difference between them
+# constraint being the higher number must come after the lower number\
+
+def stock_picker(array)
+  best_days =[0,0]
+  max_dif = 0
+  array.each_with_index do |value,indx|
+    dif = (array[indx..].max - value)
+    if dif > max_dif
+      max_dif = dif
+      best_days[0]=indx
+      best_days[1] = array.index(array[indx..].max)
+    end
+  end
+  return best_days
+end
+
+p stock_picker([1,2,3,9,5,6,7,8])
